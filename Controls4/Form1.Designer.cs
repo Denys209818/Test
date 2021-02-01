0d623042a813fedf1358734b7301fcf00b4afa26
elik <elik@example.com>
2021-02-01 18:19:09 +0200

    Add button 4

--- Controls4/Form1.Designer.cs
+++ Controls4/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace Controls4
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -61,6 +62,15 @@ namespace Controls4
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(258, 304);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(263, 134);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "button1";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
@@ -68,6 +78,7 @@ namespace Controls4
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -80,6 +91,7 @@ namespace Controls4
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
