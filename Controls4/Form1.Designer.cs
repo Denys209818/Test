e6c8cfd5b81bffa08c4b5a82dd77f07dd58b6a30
elik <elik@example.com>
2021-02-01 18:26:45 +0200

    adawfawwg

--- Controls4/Form1.Designer.cs
+++ Controls4/Form1.Designer.cs
@@ -32,8 +32,6 @@ namespace Controls4
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -64,24 +62,6 @@ namespace Controls4
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(258, 304);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(263, 134);
-            this.button4.TabIndex = 0;
-            this.button4.Text = "button1";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(539, 135);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(263, 134);
-            this.button5.TabIndex = 0;
-            this.button5.Text = "button1";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // button6
             // 
             this.button6.Location = new System.Drawing.Point(-20, 144);
@@ -99,8 +79,6 @@ namespace Controls4
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -113,8 +91,6 @@ namespace Controls4
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
     }
 }
